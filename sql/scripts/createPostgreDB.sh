3e0fb322be8978f7271bd4874255241b8005a7be
Alexander Hernandez <ahern236@wch135-32.cs.ucr.edu>
2022-11-19 16:25:22 -0800

    consolidated createPostgreDB.sh and create_db.sh

--- sql/scripts/createPostgreDB.sh
+++ sql/scripts/createPostgreDB.sh
@@ -2,3 +2,8 @@
 echo "creating db named ... "$USER"_DB"
 createdb -h localhost -p $PGPORT $USER"_DB"
 pg_ctl status
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+psql -h localhost -p $PGPORT $USER"_DB" < $DIR/../src/create_tables.sql
+psql -h localhost -p $PGPORT $USER"_DB" < $DIR/../src/create_indexes.sql
+psql -h localhost -p $PGPORT $USER"_DB" < $DIR/../src/load_data.sql
