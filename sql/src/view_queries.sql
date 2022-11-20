fb331145c0a1fe4ab711116598a44b8536bfc299
Arzhang Valadkhani <avala022@wch129-40.cs.ucr.edu>
2022-11-20 10:36:38 -0800

    First two queries

---
+++ sql/src/view_queries.sql
@@ -0,0 +1,22 @@
+-- view stores
+
+-- view products
+SELECT *
+FROM product;
+-- view recent orders
+
+-- view recent updates
+
+SELECT p.updateNumber, p.updatedOn
+FROM productUpdates p
+LEFT OUTER JOIN productUpdates p1
+ON p.updateNumber = p1.updateNumber
+AND p.updatedOn < p1.updatedOn
+WHERE p1.updateNumber IS NULL
+ORDER BY p.updatedOn asc
+FETCH FIRST 10 ROWS ONLY;
+
+-- view popular products
+
+-- view popular customers
+
