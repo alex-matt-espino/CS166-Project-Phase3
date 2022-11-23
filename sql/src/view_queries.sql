d3cf488b1d8dd13d2fee702b04bde7feb0a7a0b5
Alex Espino <alex-matt-espino@users.noreply.github.com>
2022-11-22 16:37:09 -0800

    Updated view_queries.sql
    Added completed queries 3, 5 and draft of 1

--- sql/src/view_queries.sql
+++ sql/src/view_queries.sql
@@ -1,13 +1,24 @@
 -- view stores
+-- SELECT S.storeID, S.name
+-- FROM Store S
+-- WHERE
+--pass in user lat/long,
 
--- view products
+-- view products *
 SELECT *
 FROM product;
--- view recent orders
 
--- view recent updates
+-- view recent orders *
+SELECT O.storeID, S.name, O.productName, O.unitsOrdered, O.orderTime
+FROM Orders O, Store S, Users U
+WHERE O.storeID = S.storeID AND
+U.userID = O.customerID AND
+U.userID = '32' --in Retail.java, pass in stored userID (store after login)
+GROUP BY O.storeID, S.name, O.productName, O.unitsOrdered, O.orderTime
+ORDER BY 5 DESC
+LIMIT 5;
 
-
+-- view recent updates *
 SELECT p.updateNumber, p.updatedOn
 FROM productUpdates p
 LEFT OUTER JOIN productUpdates p1
@@ -17,7 +28,27 @@ WHERE p1.updateNumber IS NULL
 ORDER BY p.updatedOn asc
 FETCH FIRST 10 ROWS ONLY;
 
--- view popular products
+
+-- view popular products *
+SELECT *
+FROM (SELECT P.productName, COUNT(O.productName) AS total_orders
+      FROM Product P, Orders O, Users U, Store S
+      WHERE U.userID = S.managerID AND --check if user is manager
+      P.storeID = S.storeID AND
+      O.storeID = S.storeID AND
+      P.productName = O.productName AND
+      S.managerID = '40' --in Retail.java, pass in stored userID (store after login, check if manager)
+      GROUP BY P.productName) AS popular_products
+ORDER BY 2 DESC
+LIMIT 5;
+
+--test
+-- SELECT DISTINCT O.productName, COUNT(O.productName)
+-- FROM Orders O, Store S, Users U
+-- WHERE S.managerID = '25' AND
+-- S.storeID = O.storeID
+-- GROUP BY O.productName
+-- ORDER BY 2 DESC
+-- LIMIT 10;
 
 -- view popular customers
-
