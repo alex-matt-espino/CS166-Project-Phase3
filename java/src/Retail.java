2f99da233df9d1c3c26d51678023e35e239ae43d
Alexander Hernandez <ahern236@wch135-32.cs.ucr.edu>
2022-12-05 14:00:51 -0800

    Updated PopularCustomers layout

--- java/src/Retail.java
+++ java/src/Retail.java
@@ -907,15 +907,15 @@ public class Retail {
             return;
          }
          String query1 = "SELECT * FROM (SELECT O.customerID, SUM(O.unitsOrdered) AS total_units_ordered FROM Product P, Orders O, Users U, Store S WHERE (U.userID = S.managerID OR U.type = \'admin\') AND P.storeID = S.storeID AND O.storeID = S.storeID AND P.productName = O.productName AND U.name = \'";
-         String query2 = "\' GROUP BY O.customerID) AS popular_products ORDER BY 2 DESC LIMIT 5;";
+         String query2 = "\' GROUP BY O.customerID) AS popular_customers ORDER BY 2 DESC LIMIT 5;";
          String query = query1 + username + query2;
          List<List<String>> popularCustomers = esql.executeQueryAndReturnResult(query);
          List<String> customer = new ArrayList<String>();
-         System.out.print("\n\t=====Popular Products=====\n");
-         System.out.print("\n|customerID|\t\t |unitsOrdered|");
+         System.out.print("\n\t=====Popular Customers=====\n");
+         System.out.print("\n|Customer ID|\t\t |# Products Ordered|");
          for(int i = 0; i < popularCustomers.size(); i++){
             customer = popularCustomers.get(i);
-            System.out.printf("\n%s %s", customer.get(0), customer.get(1));
+            System.out.printf("\n   %s \t\t\t\t%s", customer.get(0), customer.get(1));
          }
       }catch(Exception e){
          System.err.println(e.getMessage());
